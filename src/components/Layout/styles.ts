0624f67819dfcb9d3f1e58197d66ccf3231ed0e8
Gabriek0 <gabriel.sanches170@gmail.com>
2022-08-06 17:09:39 -0300

    feat: add columns in layout component and add space between them

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  main {
+    .left-column,
+    .right-column {
+      display: none;
+    }
+  }
+
+  @media (min-width: 1180px) {
+    > main {
+      display: flex;
+      justify-content: center;
+      margin: 0 30px;
+
+      .left-column,
+      .right-column {
+        display: unset;
+      }
+
+      .middle-column {
+        margin: 0 25px 16px;
+      }
+    }
+  }
+`;
